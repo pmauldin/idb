158d1e00a13e390ba988a23338a026b2cc1b5fcb
tireymorris <tireymorris@gmail.com>
2017-03-21 18:15:18 -0500

    Updated Flask server to allow CORS and changed API root to /api

--- marveldb-server/app.py
+++ marveldb-server/app.py
@@ -1,31 +1,29 @@
 from flask import Flask, jsonify
 from data_machine import characters_data, comics_data, creators_data, series_data
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}});
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-@app.route('/characters', methods = ['GET'])
+@app.route('/api/characters', methods = ['GET'])
 def characters():
 	char_json = characters_data()
 	return jsonify(char_json)
 
-@app.route('/comics', methods = ['GET'])
+@app.route('/api/comics', methods = ['GET'])
 def comics():
 	comics_json = comics_data()
 	return jsonify(comics_json)
 
-@app.route('/creators', methods = ['GET'])
+@app.route('/api/creators', methods = ['GET'])
 def creators():
 	creators_json = creators_data()
 	return jsonify(creators_json)
 
-@app.route('/series', methods = ['GET'])
+@app.route('/api/series', methods = ['GET'])
 def series():
 	series_json = series_data()
 	return jsonify(series_json)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='localhost', port=8080, debug=True)
