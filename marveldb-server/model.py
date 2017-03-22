0b2fe6bfad9ffde6e63e4b2bb10470b2d01e00a5
Jorge Zapien-Diaz <zapien256@utexas.edu>
2017-03-21 21:06:40 -0500

    Halfway done with the models

--- marveldb-server/model.py
+++ marveldb-server/model.py
@@ -8,28 +8,149 @@ class Characters(db.Model):
     __tablename__ = 'characters'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    author = db.Column(db.String(255))
-    publishedDate = db.Column(db.String(255))
-    imageUrl = db.Column(db.String(255))
-    description = db.Column(db.String(4096))
-    createdBy = db.Column(db.String(255))
-    createdById = db.Column(db.String(255))
+    character_name = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    thumbnail = db.Column(db.String(255))
+    details = db.Column(db.String(255))
+    wiki = db.Column(db.String(255))
+    comic_appearances = db.Column(db.Integer)
+    num_comic_appearances = db.Column(db.Integer)
+    series_appearances = db.Column(db.Integer)
+    num_series_appearances = db.Column(db.Integer)
+    
+    comics_id = db.Column(db.Integer, db.ForeignKey('comics.id'))
+    comics = db.relationship('Comics', backref=db.backref('characters', lazy='dynamic'))
+    
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
+    series = db.relationship('Series', backref=db.backref('characters', lazy='dynamic'))
 
-    def __repr__(self):
-        return "<Book(title='%s', author=%s)" % (self.title, self.author)
+    # title = db.Column(db.String(255))
+    # author = db.Column(db.String(255))
+    # publishedDate = db.Column(db.String(255))
+    # imageUrl = db.Column(db.String(255))
+    # description = db.Column(db.String(4096))
+    # createdBy = db.Column(db.String(255))
+    # createdById = db.Column(db.String(255))
+
+    def __init__(self, character_name, comics, series):
+        self.character_name = character_name
+        self.comics = comics
+        self.series = series
+
+    # def __repr__(self):
+    #     return '<Character %r>' % self.character_name
+
 # [END model]
 
+# [START model]
 class Comics(db.Model):
+    __tablename__ = 'comics'
 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    issuenumber = db.column(db.Integer)
+    description = db.Column(db.String(255))
+    format = db.Column(db.String(255))
+    pageCount = db.Column(db.Integer)
+    printPrice = db.Column(db.Float)
+    digitalPrice = db.Column(db.Float)
+    dateReleased = db.Column(db.String(255))
+    thumbnail = db.Column(db.String(255))
+    images = db.Column(db.String(255))
+    details = db.Column(db.String(255))
+    series = db.Column(db.Integer)
+    characters = db.Column(db.Integer)
+    numCharacters = db.Column(db.Integer)
+    creators = db.Column(db.Integer)
+    numCreators = db.Column(db.Integer)
+    
+    creator_id = db.Column(db.Integer, db.ForeignKey('creator.id'))
+    creator = db.relationship('Creators', backref=db.backref('comics', lazy='dynamic'))
+    
+    chars_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    characters = db.relationship('Characters', backref=db.backref('comics', lazy='dynamic'))
+    
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
+    series = db.relationship('Series', backref=db.backref('comics', lazy='dynamic'))
+
+    def __init__(self, title, creator, characters, series):
+        self.title = title
+        self.creator = creator
+        self.characters = characters
+        self.series = series
+
+# [END model]
+
+# [START model]
 class Creators(db.Model):
+    __tablename__ = 'creators'
 
+    id = db.Column(db.Integer, primary_key=True)
+    fullName = db.Column(db.String(255))
+    thumbnail = db.Column(db.String(255))
+    details =  db.Column(db.String(255))
+    comics = db.Column(db.Integer)
+    numComics = db.Column(db.Integer)
+    series = db.Column(db.Integer)
+    numSeries = db.Column(db.Integer)
+
+    
+    comics_id = db.Column(db.Integer, db.ForeignKey('comics.id'))
+    comics = db.relationship('Comics', backref=db.backref('creators', lazy='dynamic'))
+    
+    series_id = db.Column(db.Integer, db.ForeignKey('series.id'))
+    series = db.relationship('Series', backref=db.backref('creators', lazy='dynamic'))
+
+
+    def __init__(self, creator_name, comics, series):
+        self.creator_name = creator_name
+        self.comics = comics
+        self.series = series
+
+# [END model]
+
+# [START model]
 class Series(db.Model):
+    __tablename__ = 'series'
 
-class Char_Comics(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    startYear = db.Column(db.Integer)
+    endYear = db.Column(db.Integer)
+    rating = None
+    thumbnail = db.Column(db.String(255))
+    details = db.Column(db.String(255))
+    predecessor = db.Column(db.Integer)
+    successor = db.Column(db.Integer)
+    comics = db.Column(db.Integer)
+    numComics = db.Column(db.Integer)
+    characters = db.Column(db.Integer)
+    numCharacters = db.Column(db.Integer)
+    creators = db.Column(db.Integer)
+    numCreators = db.Column(db.Integer)
+    
+    creator_id = db.Column(db.Integer, db.ForeignKey('creator.id'))
+    creator = db.relationship('Creators', backref=db.backref('series', lazy='dynamic'))
+    
+    chars_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    characters = db.relationship('Characters', backref=db.backref('series', lazy='dynamic'))
 
-class Char_Series(db.Model):
+    comics_id = db.Column(db.Integer, db.ForeignKey('comics.id'))
+    comics = db.relationship('Characters', backref=db.backref('series', lazy='dynamic'))
 
-class Creators_Comics(db.Model):
+    def __init__(self, series_name, creator, characters, comics):
+        self.series_name = series_name
+        self.creator = creator
+        self.characters = characters
+        self.comics = comics
+    
+# [END model]
 
-class Creators_Series(db.Model):
+# class Char_Comics(db.Model):
+
+# class Char_Series(db.Model):
+
+# class Creators_Comics(db.Model):
+
+# class Creators_Series(db.Model):
